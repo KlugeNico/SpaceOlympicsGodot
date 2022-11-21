8ba27583f6a9da907fc94060a24bc57a240f3717
KlugeNico <klugenico@mailbox.org>
2022-11-21 15:47:51 +0100

    Fix build_and_deploy and add complete apple and linux support

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+
 ANDROID_SDK_ROOT="/data/android/sdk"
 godot_version="3.5.2.rc"
 project_folder="../SpaceTournament"
 
-godot_folder="$(pwd)"
-bin_folder="$godot_folder/bin"
 template_folder="$HOME/.local/share/godot/templates/$godot_version"
 export ANDROID_SDK_ROOT
+
+summary=""
 
 
 printf "Your OS: "
@@ -35,21 +36,26 @@ EOF
 
 
 clean_up() {
-	cd "$godot_folder"
 	if [ -f "custom.py" ]; then
 		rm "custom.py"
 	fi
 }
 
 fail_exit() {
-	echo "$1"
+	printf "\n BUILD AND DEPLOY SUMMARY:\n%s \n \n" "$summary"
+	printf "%s \n" "$1"
 	echo "Stop Process."
 	clean_up
 	exit 1
 }
 
+success_msg() {
+	printf "%s \n" "$1"
+	summary="$summary$1 \n"
+}
+
 ensured() {
-	$@ || fail_exit "ERROR: Can not: $@"
+	"$@" || fail_exit "ERROR: Can not: " + "$@"
 }
 
 ensured_folder() {
@@ -63,12 +69,12 @@ ensured_file_content() {
 }
 
 ensured_in_dir() {
-	(cd "$1" && shift && $@) || fail_exit "ERROR: Can not '${@:2}' in '$1'"
+	(cd "$1" && shift && "$@") || fail_exit "ERROR: Can not '" "${@:2}" "' in '$1'"
 }
 
 
-build_android() {
-	echo "BUILD FOR ANDROID"
+build_and_deploy_android() {
+	echo "BUILD AND DEPLOY ANDROID: build"
 	ensured cp custom_module_config.py custom.py
 	ensured scons platform=android target=release_debug android_arch=armv7
 	ensured scons platform=android target=release_debug android_arch=arm64v8
@@ -77,79 +83,99 @@ build_android() {
 	ensured scons platform=android target=release android_arch=x86
 	ensured scons platform=android target=release android_arch=x86_64
 	ensured rm custom.py
+
+	echo "BUILD AND DEPLOY ANDROID: gradlew generateGodotTemplates"
 	ensured_in_dir platform/android/java ./gradlew generateGodotTemplates
-}
 
-deploy_android() {
-	echo "DEPLOY FOR ANDROID"
-	echo "Install godot template folder"
-	ensured_folder "$template_folder"
-	ensured cp "$bin_folder/android_source.zip" "$template_folder"
-	ensured_file_content "$template_folder/version.txt" "$godot_version"
+	echo "BUILD AND DEPLOY ANDROID: install godot template folder"
+	ensured cp "bin/android_source.zip" "$template_folder"
 
-	echo "Prepare godot_google_play_billing"
-	godot_google_play_billing_folder="$bin_folder/godot-google-play-billing"
+	echo "BUILD AND DEPLOY ANDROID: prepare godot_google_play_billing"
+	godot_google_play_billing_folder="bin/godot-google-play-billing"
 	if [ ! -d "$godot_google_play_billing_folder" ]; then
-		ensured_in_dir "$bin_folder" git clone https://github.com/godotengine/godot-google-play-billing.git
+		ensured_in_dir "bin" git clone https://github.com/godotengine/godot-google-play-billing.git
 	fi
 	ensured_in_dir "$godot_google_play_billing_folder" git checkout billing-v5
 	ensured_in_dir "$godot_google_play_billing_folder" git pull
 	ensured_folder "$godot_google_play_billing_folder/godot-google-play-billing/libs"
-	ensured cp "$bin_folder/godot-lib.release.aar" "$godot_google_play_billing_folder/godot-google-play-billing/libs"
+	ensured cp "bin/godot-lib.release.aar" "$godot_google_play_billing_folder/godot-google-play-billing/libs"
 
-	echo "Compile godot_google_play_billing"
+	echo "BUILD AND DEPLOY ANDROID: compile godot_google_play_billing"
 	ensured_in_dir "$godot_google_play_billing_folder" sed -i 's/\x0D$//' "gradlew"
 	ensured_in_dir "$godot_google_play_billing_folder" ./gradlew build
 
-	echo "Install godot_google_play_billing to project"
+	echo "BUILD AND DEPLOY ANDROID: install godot_google_play_billing to project"
 	ensured_folder "$project_folder/android"
 	ensured_folder "$project_folder/android/plugins"
 	ensured cp -r "$godot_google_play_billing_folder/godot-google-play-billing/build/outputs/aar" "$project_folder/android/plugins"
 	ensured cp "$godot_google_play_billing_folder/GodotGooglePlayBilling.gdap" "$project_folder/android/plugins"
 
-	echo "SUCCESS"
-	echo "IMPORTANT: Remember to 'Install Android Build Template...' from godot 'Project' menu!"
+	success_msg "BUILD AND DEPLOY ANDROID: SUCCESS \n IMPORTANT: Remember to 'Install Android Build Template...' from godot 'Project' menu!"
 }
 
-build_ios() {
-	Cp custom_module_config.py custom.py
+build_and_deploy_osx() {
+	echo "BUILD AND DEPLOY OSX: build"
+	ensured cp custom_module_config.py custom.py
+	ensured scons platform=osx tools=no target=release arch=x86_64 --jobs="$(sysctl -n hw.logicalcpu)"
+	ensured scons platform=osx tools=no target=release arch=arm64 --jobs="$(sysctl -n hw.logicalcpu)"
+	ensured rm custom.py
 
-	scons p=iphone tools=no target=release arch=arm
-	scons p=iphone tools=no target=release arch=arm64
-	scons p=iphone tools=no target=release arch=x86_64 ios_simulator=yes
-	scons p=iphone tools=no target=release arch=arm64 ios_simulator=yes
-	scons p=iphone tools=no target=debug arch=arm
-	scons p=iphone tools=no target=debug arch=arm64
-	scons p=iphone tools=no target=debug arch=x86_64 ios_simulator=yes
-	scons p=iphone tools=no target=debug arch=arm64 ios_simulator=yes 
+	echo "BUILD AND DEPLOY OSX: pack"
+	ensured lipo -create "bin/godot.osx.opt.x86_64" "bin/godot.osx.opt.arm64" -output "bin/godot.osx.opt.universal"
+	ensured cp -r "misc/dist/osx_template.app" "bin"
+	ensured_folder "bin/osx_template.app/Contents"
+	ensured_folder "bin/osx_template.app/Contents/MacOS"
+	ensured cp "bin/godot.osx.opt.universal" "bin/osx_template.app/Contents/MacOS/godot_osx_release.64"
+	ensured chmod +x "bin/osx_template.app/Contents/MacOS/godot_osx*"
+	ensured_in_dir "bin" zip -q -9 -r osx.zip osx_template.app
 
-	cp -r ../misc/dist/ios_xcode .
-	
-	cp libgodot.iphone.debug.arm64.a ios_xcode/libgodot.iphone.debug.xcframework/ios-arm64/libgodot.a
-	lipo -create libgodot.iphone.debug.arm64.simulator.a libgodot.iphone.debug.x86_64.simulator.a -output ios_xcode/libgodot.iphone.debug.xcframework/ios-arm64_x86_64-simulator/libgodot.a
-	cp libgodot.iphone.opt.arm64.a ios_xcode/libgodot.iphone.release.xcframework/ios-arm64/libgodot.a
-	lipo -create libgodot.iphone.opt.arm64.simulator.a libgodot.iphone.opt.x86_64.simulator.a -output  ios_xcode/libgodot.iphone.release.xcframework/ios-arm64_x86_64-simulator/libgodot.a
+	echo "BUILD AND DEPLOY OSX: deploy"
+	ensured cp "bin/osx.zip" "$template_folder"
 
-	(cd ios_xcode && zip -r "../iphone.zip" .)
-	cp iphone.zip ~/Library/Application\ Support/Godot/templates/3.5.2.rc
-	
-	Rm custom.py
-
-	git clone https://github.com/godotengine/godot-ios-plugins.git
-
-	Rmdir godot
-	ln -s ../.. godot 
-	
-	./scripts/generate_xcframework.sh inappstore release_debug 3.x
-
-	mkdir ../../../../SpaceTournament/ios/
-	mkdir ../../../../SpaceTournament/ios/plugins/
-	mkdir ../../../../SpaceTournament/ios/plugins/inappstore
-	cp -r inappstore.release_debug.xcframework ../../../../SpaceTournament/ios/plugins/inappstore/inappstore.xcframework
-	cp plugins/inappstore/inappstore.gdip ../../../SpaceTournament/ios/plugins/inappstore
+	success_msg "BUILD AND DEPLOY OSX: SUCCESS"
 }
 
+build_and_deploy_ios() {
+	echo "BUILD AND DEPLOY IOS: build"
+	ensured cp custom_module_config.py custom.py
+	ensured scons p=iphone tools=no target=release arch=arm
+	ensured scons p=iphone tools=no target=release arch=arm64
+	ensured scons p=iphone tools=no target=release arch=x86_64 ios_simulator=yes
+	ensured scons p=iphone tools=no target=release arch=arm64 ios_simulator=yes
+	ensured scons p=iphone tools=no target=debug arch=arm
+	ensured scons p=iphone tools=no target=debug arch=arm64
+	ensured scons p=iphone tools=no target=debug arch=x86_64 ios_simulator=yes
+	ensured scons p=iphone tools=no target=debug arch=arm64 ios_simulator=yes
+	ensured rm custom.py
 
+	echo "BUILD AND DEPLOY IOS: pack"
+	ensured cp -r "misc/dist/ios_xcode" "bin"
+	ensured cp "bin/libgodot.iphone.debug.arm64.a" "bin/ios_xcode/libgodot.iphone.debug.xcframework/ios-arm64/libgodot.a"
+	ensured lipo -create "bin/libgodot.iphone.debug.arm64.simulator.a" "bin/libgodot.iphone.debug.x86_64.simulator.a" -output "bin/ios_xcode/libgodot.iphone.debug.xcframework/ios-arm64_x86_64-simulator/libgodot.a"
+	ensured cp "bin/libgodot.iphone.opt.arm64.a" "bin/ios_xcode/libgodot.iphone.release.xcframework/ios-arm64/libgodot.a"
+	ensured lipo -create "bin/libgodot.iphone.opt.arm64.simulator.a" "bin/libgodot.iphone.opt.x86_64.simulator.a" -output  "bin/ios_xcode/libgodot.iphone.release.xcframework/ios-arm64_x86_64-simulator/libgodot.a"
+	ensured_in_dir "bin/ios_xcode" zip -q -9 -r "../iphone.zip" .
+
+	echo "BUILD AND DEPLOY IOS: deploy"
+	ensured cp iphone.zip "$HOME/Library/Application Support/Godot/templates/3.5.2.rc"
+
+	echo "BUILD AND DEPLOY IOS: prepare godot-ios-plugins"
+	ensured_in_dir "bin" git clone https://github.com/godotengine/godot-ios-plugins.git
+	ensured rmdir "bin/godot-ios-plugins/godot"
+	ensured_in_dir "bin/godot-ios-plugins" ln -s ../.. godot
+
+	echo "BUILD AND DEPLOY IOS: build godot-ios-plugin inappstore"
+	ensured_in_dir "bin/godot-ios-plugins" ./scripts/generate_xcframework.sh inappstore release_debug 3.x
+
+	echo "BUILD AND DEPLOY IOS: deploy godot-ios-plugin inappstore"
+	ensured_folder "$project_folder/ios"
+	ensured_folder "$project_folder/ios/plugins"
+	ensured_folder "$project_folder/ios/plugins/inappstore"
+	ensured cp -r "bin/godot-ios-plugins/build/bin/inappstore.release_debug.xcframework" "$project_folder/ios/plugins/inappstore/inappstore.xcframework"
+	ensured cp "bin/godot-ios-plugins/plugins/inappstore/inappstore.gdip" "$project_folder/ios/plugins/inappstore"
+
+	success_msg "BUILD AND DEPLOY IOS: SUCCESS"
+}
 
 
 do_x11_editor=0
@@ -160,10 +186,11 @@ do_osx=0
 do_iphone=0
 
 if [ $# -eq 0 ]; then
-  printf "Missing arguments! List desired platforms as arguments.\n\n"
+  printf "Missing arguments! List desired platforms as arguments. \n \n"
 	echo_help
 	exit 1
 fi
+
 
 # idiomatic parameter and option handling in sh
 while test $# -gt 0
@@ -202,8 +229,8 @@ do
 			do_iphone=1
 			;;
   	*)
-    	printf "Error in command line usage:\n"
-    	printf "Unknown command: %s\n\n" "$1"
+    	printf "Error in command line usage: \n"
+    	printf "Unknown command: %s \n \n" "$1"
     	echo_help
     	exit 1
     	;;
@@ -211,53 +238,46 @@ do
 	shift
 done
 
+
 clean_up
+ensured_folder "$template_folder"
+ensured_file_content "$template_folder/version.txt" "$godot_version"
+
 
 if [ $do_x11_editor -eq 1 ] ; then
-	echo $do_x11_editor
-	echo "BUILD EDITOR FOR LINUX"
-	scons platform=x11
+	echo "BUILD X11 EDITOR: build"
+	ensured scons platform=x11
+	success_msg "BUILD X11 EDITOR: SUCCESS"
 fi
 
 if [ $do_x11 -eq 1 ] ; then
-	echo "BUILD FOR LINUX"
+	echo "BUILD AND DEPLOY X11: build"
 	ensured cp custom_module_config.py custom.py
-	scons platform=x11 tools=no target=release
+	ensured scons platform=x11 tools=no target=release
 	ensured rm custom.py
+	echo "BUILD AND DEPLOY X11: deploy"
+	ensured cp "bin/godot.x11.opt.64" "$template_folder/linux_x11_64_release"
+	success_msg "BUILD X11: SUCCESS"
 fi
 
 if [ $do_android -eq 1 ] ; then
-	# build_android
-	deploy_android
+	build_and_deploy_android
 fi
 
 if [ $do_osx_editor -eq 1 ] ; then
-	echo "BUILD EDITOR FOR MAC"
-	scons platform=osx arch=x86_64 --jobs=$(sysctl -n hw.logicalcpu)  # arch=arm64 for arm cpus
+	echo "BUILD OSX EDITOR: build"
+	ensured scons platform=osx arch=x86_64 --jobs="$(sysctl -n hw.logicalcpu)"  # arch=arm64 for arm cpus
+	success_msg "BUILD OSX EDITOR: SUCCESS"
 fi
 
 if [ $do_osx -eq 1 ] ; then
-	echo "BUILD FOR MAC"
-	cp custom_module_config.py custom.py
-	scons platform=osx tools=no target=release arch=x86_64 --jobs=$(sysctl -n hw.logicalcpu)
-	scons platform=osx tools=no target=release arch=arm64 --jobs=$(sysctl -n hw.logicalcpu)
-	rm custom.py
-	lipo -create bin/godot.osx.opt.x86_64 bin/godot.osx.opt.arm64 -output bin/godot.osx.opt.universal
-	cp -r misc/dist/osx_template.app .
-	mkdir -p osx_template.app/Contents/MacOS
-	cp bin/godot.osx.opt.universal osx_template.app/Contents/MacOS/godot_osx_release.64
-	chmod +x osx_template.app/Contents/MacOS/godot_osx*
-	zip -q -9 -r osx.zip osx_template.app
+	build_and_deploy_osx
 fi
 
 if [ $do_iphone -eq 1 ] ; then
-	echo "BUILD FOR IPHONE"
-	cp custom_module_config.py custom.py
-	scons p=iphone tools=no target=release arch=arm
-	scons p=iphone tools=no target=release arch=arm64
-	rm custom.py
-	lipo -create bin/libgodot.iphone.opt.arm.a bin/libgodot.iphone.opt.arm64.a -output bin/libgodot.iphone.release.fat.a
+	build_and_deploy_ios
 fi
 
 
+printf "\n BUILD AND DEPLOY SUMMARY: \n %s \n" "$summary"
 exit 0
